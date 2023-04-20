3e2a4bbdf8f7a06823f09bb660b0d04ffeb77c60
zagob <matheusbestana@gmail.com>
2023-04-20 18:04:18 -0300

    mock button function

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
 import CalculatorImg from "../../images/Calculator.png";
 
 export function Menu() {
+  const [auth, setAuth] = useState(true);
+  const [state, setState] = useState(false);
+
+  function changeState() {
+    setAuth(false);
+  }
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <img src={CalculatorImg} alt="" />
-          Home
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <img src={CalculatorImg} alt="" />
+            Home
+          </li>
+        </ul>
+      </nav>
+
+      <button type="button" onClick={changeState}>
+        Click Me!
+      </button>
+      <p>auth: {auth}</p>
+    </>
   );
 }
--- src/components/Menu/menu.spec.tsx
+++ src/components/Menu/menu.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, renderHook } from "@testing-library/react";
 import { Menu } from ".";
 
 jest.mock("../../images/Calculator.png", () => "calculator.png");
@@ -8,5 +8,15 @@ describe("Menu component", () => {
     render(<Menu />);
     const menuComponent = screen.getByText("Home");
     expect(menuComponent).toBeInTheDocument();
+  });
+
+  it("should clicked button", () => {
+    render(<Menu />);
+    const { result } = renderHook(() => Menu());
+    const button = screen.getByText("Click Me!");
+    const p = screen.getByText("auth: false");
+    fireEvent.click(button);
+    expect(button).toBeInTheDocument();
+    expect(p).toBeInTheDocument();
   });
 });
